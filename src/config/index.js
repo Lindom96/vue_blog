61c09224e6c9af3ece008f7a98bc2d4fbd67b548
Lindom96 <1601514271@qq.com>
2022-02-21 21:50:02 +0800

    修改baseUrl

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const Config = {
     sideBarLevel: 3, // 侧边栏层级限制, 3表示三级, 可设置 2 和 3
     defaultRoute: '/login', // 默认打开的路由
     // baseUrl: 'http://127.0.0.1:3000',
-    baseUrl: 'http://lindom.blog.com/',
+    baseUrl: 'http://120.76.72.225:80',
     notLoginRoute: ['login','home'], // 无需登录即可访问的路由 name
 }
 
